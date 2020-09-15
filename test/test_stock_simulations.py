a1611e4c8fa13bd981ec39fed7450cb269192cdb
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-09-15 18:57:19 -0400

    unit tests for Black-Scholes-Merton model

--- test/test_stock_simulations.py
+++ test/test_stock_simulations.py
@@ -14,14 +14,22 @@ def expected_stock(S0, r, sigma, t):
 
 
 class TestStockSimulations(unittest.TestCase):
-    def test_BlackScholesMertonStocks(self):
-        bsm_simulator = BlackScholesMertonStockPrices(100, 0.04, 0.02)
-        sarray = bsm_simulator.generate_time_series(100, 0.1, 10000)
+    def backend_test_BlackScholesMertonStocks(self, S, r, sigma, T, dt, nbsimulations):
+        bsm_simulator = BlackScholesMertonStockPrices(S, r, sigma)
+        sarray = bsm_simulator.generate_time_series(T, dt, nbsimulations=nbsimulations)
         average_s = np.mean(sarray[:, -1])
 
-        expected_s, variance_s = expected_stock(100, 0.04, 0.01, 100)
+        expected_s, variance_s = expected_stock(S, r, sigma, T)
 
         self.assertAlmostEqual(average_s, expected_s, delta=1.96*sqrt(variance_s))
+
+    def test_test_BlackScholesMertonStocks(self):
+        self.backend_test_BlackScholesMertonStocks(100, 0.04, 0.02, 100, 0.1, 10000)
+
+        for S in np.random.lognormal(100, 10, size=3):
+            for r in np.random.lognormal(0.04, 0.01, size=3):
+                for sigma in np.random.lognormal(0.01, 0.0001, size=3):
+                    self.backend_test_BlackScholesMertonStocks(S, r, sigma, 100, 0.1, 10000)
 
 
 if __name__ == '__main__':
