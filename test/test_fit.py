0ac5806abc39cb9291cd88bf8cfc86eb4b687a08
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-09-22 14:20:21 -0400

    unit test on parameter estimation

---
+++ test/test_fit.py
@@ -0,0 +1,37 @@
+
+import unittest
+from datetime import datetime, timedelta
+from math import sqrt
+
+import numpy as np
+
+from finsim.simulation.stock import BlackScholesMertonStockPrices
+from finsim.data.fit import fit_BlackScholesMerton_model
+
+
+class TestParameterFitting(unittest.TestCase):
+    def test_fit(self):
+        bsm_simulator = BlackScholesMertonStockPrices(100, 0.04, 0.02)
+        prices = bsm_simulator.generate_time_series(100, 0.1, nbsimulations=1000)
+        timestamps = np.array(
+            [
+                datetime(2020, 1, 1, 0, 0, 0) + i*timedelta(1)
+                for i in range(100)
+            ],
+            dtype='datetime64[s]'
+        )
+        info = [
+            (r, sigma)
+            for r, sigma in map(lambda i: fit_BlackScholesMerton_model(prices[i, :]), range(1000))
+        ]
+        mean_r = np.mean([item[0] for item in info])
+        mean_sigma = np.mean([item[1] for item in info])
+
+        self.assertAlmostEqual(mean_r, 0.04, delta=1.96*0.02/sqrt(1000))
+        self.assertAlmostEqual(mean_sigma, 0.02, delta=1.96*0.02/sqrt(1000))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test/test_stock_simulations.py
+++ test/test_stock_simulations.py
@@ -21,7 +21,7 @@ class TestStockSimulations(unittest.TestCase):
 
         expected_s, variance_s = expected_bsm_stock(S, r, sigma, T)
 
-        self.assertAlmostEqual(average_s, expected_s, delta=1.96*sqrt(variance_s))
+        self.assertAlmostEqual(average_s, expected_s, delta=1.96*sqrt(variance_s)/sqrt(nbsimulations))
 
     def test_BlackScholesMertonStocks(self):
         self.backend_test_BlackScholesMertonStocks(100, 0.04, 0.02, 100, 0.1, 10000)
@@ -41,27 +41,27 @@ class TestStockSimulations(unittest.TestCase):
         self.assertAlmostEqual(
             np.mean([stock[100] for stock in h_stocks]),
             110.52,
-            delta=1.96*0.11
+            delta=1.96*0.11/sqrt(1000)
         )
         self.assertAlmostEqual(
             np.mean([stock[250] for stock in h_stocks]),
             128.41,
-            delta=1.96*0.23
+            delta=1.96*0.23/sqrt(1000)
         )
         self.assertAlmostEqual(
             np.mean([stock[500] for stock in h_stocks]),
             164.92,
-            delta=1.96*0.38
+            delta=1.96*0.38/sqrt(1000)
         )
         self.assertAlmostEqual(
             np.mean([stock[750] for stock in h_stocks]),
             211.80,
-            delta=1.96*0.56
+            delta=1.96*0.56/sqrt(1000)
         )
         self.assertAlmostEqual(
             np.mean([stock[999] for stock in h_stocks]),
             271.72,
-            delta=1.96*0.80
+            delta=1.96*0.80/sqrt(1000)
         )
 
     def test_MertonJumpDiffusionStocks(self):
@@ -74,22 +74,22 @@ class TestStockSimulations(unittest.TestCase):
         self.assertAlmostEqual(
             np.mean([stock[250] for stock in mjd_stocks]),
             197.97,
-            delta=1.96*6.37
+            delta=1.96*6.37/sqrt(1000)
         )
         self.assertAlmostEqual(
             np.mean([stock[500] for stock in mjd_stocks]),
             393.35,
-            delta=1.96*28.15
+            delta=1.96*28.15/sqrt(1000)
         )
         self.assertAlmostEqual(
             np.mean([stock[750] for stock in mjd_stocks]),
             773.20,
-            delta=1.96*67.73
+            delta=1.96*67.73/sqrt(1000)
         )
         self.assertAlmostEqual(
             np.mean([stock[999] for stock in mjd_stocks]),
             1523.06,
-            delta=1.96*169.68
+            delta=1.96*169.68/sqrt(1000)
         )
 
 
