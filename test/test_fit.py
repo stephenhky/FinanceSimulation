d56f2bc37450cb5631bbb6f17b757c49ea6c947a
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-09-22 14:46:21 -0400

    unit test on parameter estimation (fixed)

--- test/test_fit.py
+++ test/test_fit.py
@@ -12,17 +12,17 @@ from finsim.data.fit import fit_BlackScholesMerton_model
 class TestParameterFitting(unittest.TestCase):
     def test_fit(self):
         bsm_simulator = BlackScholesMertonStockPrices(100, 0.04, 0.02)
-        prices = bsm_simulator.generate_time_series(100, 0.1, nbsimulations=1000)
+        prices = bsm_simulator.generate_time_series(100, 1, nbsimulations=1000)
         timestamps = np.array(
             [
                 datetime(2020, 1, 1, 0, 0, 0) + i*timedelta(1)
-                for i in range(100)
+                for i in range(101)
             ],
             dtype='datetime64[s]'
         )
         info = [
             (r, sigma)
-            for r, sigma in map(lambda i: fit_BlackScholesMerton_model(prices[i, :]), range(1000))
+            for r, sigma in map(lambda i: fit_BlackScholesMerton_model(timestamps, prices[i, :], unit='day'), range(1000))
         ]
         mean_r = np.mean([item[0] for item in info])
         mean_sigma = np.mean([item[1] for item in info])
