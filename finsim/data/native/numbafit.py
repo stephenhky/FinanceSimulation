0b7541fc1f2649f385a9b7d0cd3fa0875fec490d
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-09-23 00:02:22 -0400

    numba on datetime object failed. need more optimization

--- finsim/data/native/numbafit.py
+++ finsim/data/native/numbafit.py
@@ -3,10 +3,10 @@ import numpy as np
 import numba as nb
 
 
-@nb.njit(nb.types.Tuple(nb.float64, nb.float64)(nb.datetime[:], nb.float64[:], nb.int64))
-def numba_fit_BlackScholesMerton_model(timestamps, prices, dividing_factor):
+@nb.jit(nb.types.Tuple((nb.float64, nb.float64))(nb.types.NPDatetime('s')[:], nb.float64[:], nb.int64))
+def numba_fit_BlackScholesMerton_model(ts, prices, dividing_factor):
     dlogS = np.log(prices[1:] / prices[:-1])
-    dt = np.array(timestamps[1:] - timestamps[:-1], dtype='timedelta64[s]')
+    dt = np.array(ts[1:] - ts[:-1], dtype='timedelta64[s]')
     dt = np.array(dt, dtype=np.float) / dividing_factor
 
     r = np.mean(dlogS / dt)
@@ -15,10 +15,10 @@ def numba_fit_BlackScholesMerton_model(timestamps, prices, dividing_factor):
     return r, sigma
 
 
-@nb.njit(nb.types.Tuple(nb.float64[:], nb.float64[:, :])(nb.datetime[:], nb.float64[:, :], nb.int64))
-def numba_fit_multivariate_BlackScholesMerton_model(timestamps, multiprices, dividing_factor):
+@nb.jit(nb.types.Tuple((nb.float64[:], nb.float64[:, :]))(nb.types.NPDatetime('s')[:], nb.float64[:, :], nb.int64))
+def numba_fit_multivariate_BlackScholesMerton_model(ts, multiprices, dividing_factor):
     dlogS = np.log(multiprices[:, 1:] / multiprices[:, :-1])
-    dt = np.array(timestamps[1:] - timestamps[:-1], dtype='timedelta64[s]')
+    dt = np.array(ts[1:] - ts[:-1], dtype='timedelta64[s]')
     dt = np.array(dt, dtype=np.float) / dividing_factor
 
     r = np.mean(dlogS / dt, axis=1)
