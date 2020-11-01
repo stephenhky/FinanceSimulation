e21055d04b8afab50bd142cad2aa739dfbfc23f8
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-11-01 12:02:48 -0500

    release 0.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def package_description():
 
 setup(
     name='finsim',
-    version="0.1.0a1",
+    version="0.1.0",
     description="Financial simulation and inference",
     long_description=package_description(),
     long_description_content_type='text/markdown',
