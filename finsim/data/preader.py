b4910982e1ecb16a30c3908524e2bbf01ba44bf5
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-11-01 11:20:47 -0500

    caching symbol information

--- finsim/data/preader.py
+++ finsim/data/preader.py
@@ -1,14 +1,16 @@
 
 from datetime import datetime
-import sys
+import os
 import logging
 
 import pandas as pd
 from pandas_datareader import data
 from pandas_datareader._utils import RemoteDataError
+import tables as tb
 
 
-def get_yahoofinance_data(symbol, startdate, enddate):
+# native yahoo reader
+def extract_online_yahoofinance_data(symbol, startdate, enddate):
     try:
         df = data.DataReader(
             name=symbol,
@@ -34,3 +36,66 @@ def get_yahoofinance_data(symbol, startdate, enddate):
     df = df[['TimeStamp'] + list(oricols)]
 
     return df
+
+
+# yahoo reader with local cache
+METATABLE_FILENAME = 'SYMBOL_CACHE.h5'
+METATABLE_ROWDES = {
+    'symbol': tb.StringCol(shape=(), itemsize=10, dflt='', pos=0),
+    'query_startdate': tb.StringCol(shape=(), itemsize=10, dflt='0000-00-00', pos=1),
+    'query_enddate': tb.StringCol(shape=(), itemsize=10, dflt='0000-00-00', pos=2),
+    'data_startdate': tb.StringCol(shape=(), itemsize=10, dflt='0000-00-00', pos=3),
+    'data_enddate': tb.StringCol(shape=(), itemsize=10, dflt='0000-00-00', pos=4)
+}
+
+
+def get_yahoofinance_data(symbol, startdate, enddate, cacheddir=None):
+    if cacheddir is None:
+        return extract_online_yahoofinance_data(symbol, startdate, enddate)
+
+    if isinstance(cacheddir, str):
+        if not os.path.exists(cacheddir) or (os.path.exists(cacheddir) and not os.path.isdir(cacheddir)):
+            logging.info('Creating directory: {}'.format(cacheddir))
+            os.makedirs(cacheddir)
+        if not os.path.exists(os.path.join(cacheddir, METATABLE_FILENAME)):
+            logging.info('Creating file: {}'.format(os.path.join(cacheddir, METATABLE_FILENAME)))
+            metatable_h5file = tb.open_file(os.path.join(cacheddir, METATABLE_FILENAME), 'w')
+            table = metatable_h5file.create_table('/', 'metatable', METATABLE_ROWDES, title='metatable')
+        else:
+            metatable_h5file = tb.open_file(os.path.join(cacheddir, METATABLE_FILENAME), 'r+')
+            table = metatable_h5file.root.metatable
+
+        preexist = False
+        for row in table.where('symbol=="{}"'.format(symbol)):
+            preexist = True
+            if row['query_startdate'] >= startdate and row['query_enddate'] <= enddate:
+                df = pd.read_hdf(os.path.join(cacheddir, '{}.h5'.format(symbol)), 'yahoodata')
+                df = df[ (df['TimeStamp']>=startdate) & (df['TimeStamp']<=enddate) ]
+                return df
+
+        df = extract_online_yahoofinance_data(symbol, startdate, enddate)
+        logging.debug('Caching data for {} from {} to {}'.format(symbol, startdate, enddate))
+        df.to_hdf(os.path.join(cacheddir, '{}.h5'.format(symbol)), 'yahoodata')
+
+        if preexist:
+            logging.debug('Updating symbol {} in metatable.'.format(symbol))
+            for row in table.where('symbol=="{}"'.format(symbol)):
+                row['query_startdate'] = startdate
+                row['query_enddate'] = enddate
+                row['data_startdate'] = datetime.strftime(df['TimeStamp'][0].date(), '%Y-%m-%d')
+                row['data_enddate'] = datetime.strftime(df['TimeStamp'][-1].date(), '%Y-%m-%d')
+                row.update()
+        else:
+            logging.debug('Creating symbol {} in metatable'.format(symbol))
+            newrow = table.row
+            newrow['symbol'] = symbol
+            newrow['query_startdate'] = startdate
+            newrow['query_enddate'] = enddate
+            newrow['data_startdate'] = datetime.strftime(df['TimeStamp'][0].date(), '%Y-%m-%d')
+            newrow['data_enddate'] = datetime.strftime(df['TimeStamp'][-1].date(), '%Y-%m-%d')
+            newrow.append()
+
+        table.flush()
+        metatable_h5file.close()
+
+        return df
