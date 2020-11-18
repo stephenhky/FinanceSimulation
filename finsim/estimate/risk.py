fixing problems about timestamps units

finsim/estimate/risk.py
@@ -1,6 +1,14 @@
 
+import numpy as np
+
 from .native.numbarisk import numba_estimate_downside_risk
+from .constants import dividing_factors_dict
 
 
-def estimate_downside_risk(ts, prices, target_return):
+def estimate_downside_risk(timestamps, prices, target_return, unit='year'):
+    dividing_factor = dividing_factors_dict[unit]
+
+    ts = np.array(timestamps, dtype='datetime64[s]')
+    ts = np.array(ts, dtype=np.float) / dividing_factor
+
     return numba_estimate_downside_risk(ts, prices, target_return)