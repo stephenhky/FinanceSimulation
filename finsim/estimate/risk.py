6cc470f5e6a9e6a1146aeda6a3df0d3998a81652
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-11-18 21:34:10 -0500

    debugged on risks

--- finsim/estimate/risk.py
+++ finsim/estimate/risk.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-from .native.numbarisk import numba_estimate_downside_risk
+from .native.numbarisk import numba_estimate_downside_risk, numba_estimate_upside_risk
 from .constants import dividing_factors_dict
 
 
@@ -12,3 +12,12 @@ def estimate_downside_risk(timestamps, prices, target_return, unit='year'):
     ts = np.array(ts, dtype=np.float) / dividing_factor
 
     return numba_estimate_downside_risk(ts, prices, target_return)
+
+
+def estimate_upside_risk(timestamps, prices, target_return, unit='year'):
+    dividing_factor = dividing_factors_dict[unit]
+
+    ts = np.array(timestamps, dtype='datetime64[s]')
+    ts = np.array(ts, dtype=np.float) / dividing_factor
+
+    return numba_estimate_upside_risk(ts, prices, target_return)
