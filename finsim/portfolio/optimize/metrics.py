4c1fab34fc7ef9b991d2e7bae3719e2449c24d1c
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-12-01 00:34:26 -0500

    no numba

--- finsim/portfolio/optimize/metrics.py
+++ finsim/portfolio/optimize/metrics.py
@@ -1,9 +1,7 @@
 
 import numpy as np
-import numba as nb
 
 
-@nb.njit(nb.float64(nb.float64[:], nb.float64[:], nb.float64[:, :], nb.float64))
 def sharpe_ratio(weights, r, cov, rf):
     yieldrate = np.sum(weights * r)
     sqweights = np.matmul(
