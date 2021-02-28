bfe00704b67d861309206e14fa076bd136f1faa8
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-02-28 12:20:26 -0500

    debugging

--- finsim/portfolio/dynamic.py
+++ finsim/portfolio/dynamic.py
@@ -8,6 +8,10 @@ import pandas as pd
 from .portfolio import Portfolio
 
 
+class InsufficientSharesException(Exception):
+    pass
+
+
 class DynamicPortfolio(Portfolio):
     def __init__(self, symbol_nbshares, current_date, cacheddir=None):
         # current_date is a string, of format '%Y-%m-%d', such as '2020-02-23'
@@ -55,6 +59,7 @@ class DynamicPortfolio(Portfolio):
         # date is a string, of format '%Y-%m-%d', such as '2020-02-23'
         self.timeseriesidx = self.find_cursor_for_date(date)
         self.symbols_nbshares = self.timeseries[self.timeseriesidx]['portfolio'].symbols_nbshares.copy()
+        logging.debug('MOVED!')
 
     def trade(
             self,
@@ -82,27 +87,41 @@ class DynamicPortfolio(Portfolio):
 
         symbols_nbshares = defaultdict(lambda : 0, buy_stocks)
         if check_valid:
+            logging.debug('CHECK VALID')
+            logging.debug(self.symbols_nbshares)
             if raise_insufficient_stock_error:
                 errorfound = False
                 for symbol, nbshares in sell_stocks.items():
-                    if nbshares < self.symbols_nbshares[symbol]:
+                    logging.debug('{}: {} < {}'.format(symbol, nbshares, self.symbols_nbshares[symbol]))
+                    logging.debug(nbshares > self.symbols_nbshares[symbol])
+                    if nbshares > self.symbols_nbshares[symbol]:
                         logging.error('Insufficient stock {} ({} shares, but want to sell {} shares)'.format(symbol, self.symbols_nbshares[symbol], nbshares))
                         errorfound = True
                 if errorfound:
-                    raise ValueError('Insufficient stocks.')
+                    raise InsufficientSharesException('Insufficient stocks.')
             else:
                 for symbol, nbshares in sell_stocks.items():
-                    if nbshares < self.symbols_nbshares[symbol]:
+                    logging.debug('{}: {} < {}'.format(symbol, nbshares, self.symbols_nbshares[symbol]))
+                    logging.debug(nbshares > self.symbols_nbshares[symbol])
+                    if nbshares > self.symbols_nbshares[symbol]:
+                        logging.debug('UPDATE sell_stocks')
                         sell_stocks[symbol] = self.symbols_nbshares[symbol]
 
         for symbol, nbshares in sell_stocks.items():
             symbols_nbshares[symbol] -= nbshares
+        # logging.debug(symbols_nbshares)
 
         difference_portfolio = Portfolio(symbols_nbshares, cacheddir=self.cacheddir)
         self.timeseries.append({
             'date': trade_date,
             'portfolio': self.timeseries[-1]['portfolio'] + difference_portfolio
         })
+
+        self.move_cursor_to_date(trade_date)
+        
+    def get_portfolio_value(self, datestr):
+        idx = self.find_cursor_for_date(datestr)
+        return self.timeseries[idx]['portfolio'].get_portfolio_value(datestr)
 
     def get_portfolio_values_overtime(self, startdate, enddate, cacheddir=None):
         assert self.is_sorted()
