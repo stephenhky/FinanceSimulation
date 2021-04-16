fbd1906c580d581023f7443233b6488da5453cdb
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-04-16 17:03:39 -0400

    initialize the use of Cython

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def package_description():
 
 setup(
     name='finsim',
-    version="0.6.5",
+    version="0.6.6a1",
     description="Financial simulation and inference",
     long_description=package_description(),
     long_description_content_type='text/markdown',
