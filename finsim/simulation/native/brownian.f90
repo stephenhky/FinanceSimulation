ae93e89cee35515e7320bfc4ec13c99292ca515b
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-04-29 14:10:11 -0400

    working random number generator for normal distribution

--- finsim/simulation/native/brownian.f90
+++ finsim/simulation/native/brownian.f90
@@ -3,31 +3,40 @@ module f90brownian
     private
     public inv_errfcn, normaldistsampling
 
-    real, parameter :: pi = 3.141592653589793
-    real, parameter :: erfcoef3 = pi/12.
-    real, parameter :: erfcoef5 = 7.*pi*pi/480.
-    real, parameter :: erfcoef7 = 127.*pi*pi*pi/40320.
-    real, parameter :: erfcoef9 = 4369.*pi*pi*pi*pi/5806080.
-    real, parameter :: erfcoef11 = 34807.*pi*pi*pi*pi*pi/182476800.
-
-    integer :: iseed(1)
-
 contains
 
-    function inv_errfcn(u) result(z)
-        real, intent(in) :: u
+    function inv_errfcn(x) result(z)
+        real, intent(in) :: x
+        real :: w, p
         real :: z
-        real :: u2, u3, u5, u7, u9, u11
 
-        u2 = u*u
-        u3 = u*u2
-        u5 = u3*u2
-        u7 = u5*u2
-        u9 = u7*u2
-        u11 = u9*u2
+        w = -log((1.0-x)*(1.0+x))
 
-        z = u + erfcoef3*u3 + erfcoef5*u5 + erfcoef7*u7 + erfcoef9*u9 + erfcoef11*u11
-        z = 0.5 * sqrt(pi) * z
+        if (w < 5.0) then
+            w = w - 2.5
+            p = 2.81022636e-08
+            p = 3.43273939e-07 + p*w
+            p = -3.5233877e-06 + p*w
+            p = -4.39150654e-06 + p*w
+            p = 0.00021858087 + p*w
+            p = -0.00125372503 + p*w
+            p = -0.00417768164 + p*w
+            p = 0.246640727 + p*w
+            p = 1.50140941 + p*w
+        else
+            w = sqrt(w) - 3.0
+            p = -0.000200214257
+            p = 0.000100950558 + p*w
+            p = 0.00134934322 + p*w
+            p = -0.00367342844 + p*w
+            p = 0.00573950773 + p*w
+            p = -0.0076224613 + p*w
+            p = 0.00943887047 + p*w
+            p = 1.00167406 + p*w
+            p = 2.83297682 + p*w
+        end if
+
+        z = p*x
 
     end function inv_errfcn
 
@@ -37,12 +46,16 @@ contains
         real :: uniform_r, u
 
         call random_number(uniform_r)
+        write(*,*) uniform_r
         u = 2*uniform_r - 1
 
-        z = inv_errfcn(u)
+        z = inv_errfcn(u) * sqrt(2.)
 
     end function normaldistsampling
 
 
 
 end module f90brownian
+
+! reference: https://people.maths.ox.ac.uk/gilesm/files/gems_erfinv.pdf
+! normal distribution: https://en.wikipedia.org/wiki/Normal_distribution
