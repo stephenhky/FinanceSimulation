0d6071c1c052adf90a3062c759c56ae0a15ff61d
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-11-05 00:58:27 -0400

    fixing dataframe index reset problem

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def package_description():
 
 setup(
     name='finsim',
-    version="0.7.4",
+    version="0.7.5a1",
     description="Financial simulation and inference",
     long_description=package_description(),
     long_description_content_type='text/markdown',
