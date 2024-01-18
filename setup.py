a318f6c22d9dc54bfd95049775b747c9b9817c7b
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-01-17 19:20:43 -0500

    added a script to grab symbols

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     tests_require=[
       'unittest2'
     ],
-    # scripts=[],
+    script=['script/retrieve_stock_symbols'],
     include_package_data=True,
     ext_modules=ext_modules,
     test_suite="test",
