af14e1ea0cf6d06c01f2f928d4ef6f1ed7c43df7
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-05-17 18:43:27 -0400

    removed Cython supports for metrics, type hinting enforcement; deleted setup.py

--- finsim/portfolio/optimize/native/pymetrics.py
+++ finsim/portfolio/optimize/native/pymetrics.py
@@ -1,21 +1,46 @@
 
 import numpy as np
+import numpy.typing as npt
+import numba as nb
 
 
-def python_sharpe_ratio(weights, r, cov, rf):
+@nb.njit
+def python_sharpe_ratio(
+        weights: npt.NDArray[np.float64],
+        r: npt.NDArray[np.float64],
+        cov: npt.NDArray[np.float64],
+        rf: float
+) -> float:
     yieldrate = np.sum(weights * r)
     sqweights = np.expand_dims(weights, axis=1) @ np.expand_dims(weights, axis=0)
     volatility = np.sqrt(np.sum(sqweights * cov))
     return (yieldrate - rf) / volatility
 
 
-def python_mpt_costfunction(weights, r, cov, rf, lamb, V0=10.):
+@nb.njit
+def python_mpt_costfunction(
+        weights: npt.NDArray[np.float64],
+        r: npt.NDArray[np.float64],
+        cov: npt.NDArray[np.float64],
+        rf: float,
+        lamb: float,
+        V0: float=10.
+) -> float:
     weightmat = np.expand_dims(weights[:-1], axis=0)
     c = lamb * V0
     return weights[-1]*rf + np.dot(weights[:-1], r) - 0.5*c/V0*(weightmat @ cov @ weightmat.T)[0, 0]
 
 
-def python_mpt_entropy_costfunction(weights, r, cov, rf, lamb0, lamb1, V=10.):
+@nb.njit
+def python_mpt_entropy_costfunction(
+        weights: npt.NDArray[np.float64],
+        r: npt.NDArray[np.float64],
+        cov: npt.NDArray[np.float64],
+        rf: float,
+        lamb0: float,
+        lamb1: float,
+        V: float=10.
+) -> float:
     weightmat = np.expand_dims(weights[:-1], axis=0)
     c0 = lamb0 * V
     c1 = lamb1 * V
